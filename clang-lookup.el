70c82e1f6b9facf0ad8f8a70a15eb7902ade123a
Jan Rehders <cmdkeen@gmx.de>
2011-10-30 21:23:15 +0100

    Initial commit, first version of clang_lookup.exe and Emacs plugin

---
+++ clang-lookup.el
@@ -0,0 +1,90 @@
+;;; clang-lookup.el --- Goto symbol definition using clang
+
+;; Copyright 2011 Jan Rehders
+;; 
+;; Author: Jan Rehders <cmdkeen@gmx.de>
+;; Version: 0.1
+;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+;;
+;; A simple minor mode to use clang to navigate to a symbol's definition
+;; Add something like this to your .emacs:
+;;
+;; (autoload 'clang-goto-symbol-at-point "clang-lookup")
+;; (define-key 'c++-mode-map (kbd "M-.") 'clang-goto-symbol-at-point)
+;;
+;; This is a very early version lacking reasonable error handling etc.
+;;
+;; Todo:
+;; - add reasonable error handling
+;; - add customizable function to setup clang arguments (include paths, etc.)
+;; - pass compiler errors and warnings on to flymake
+;;
+
+(defcustom clang-lookup-ld-libclang-path
+  ""
+  "Path to libclang.so"
+  :type 'string
+  :group 'clang-lookup)
+
+(defcustom clang-lookup-exe
+  "clang_lookup.exe"
+  "clang_lookup executable"
+  :type 'string
+  :group 'clang-lookup)
+
+(defun clang-lookup ()
+  (interactive)
+  (let* ((file (buffer-file-name))
+         (line (1+ (current-line)))
+         (column (1+ (current-column)))
+         (output (shell-command-to-string
+                 (format "LD_LIBRARY_PATH=%s %s %s %s %s"
+                         clang-lookup-ld-libclang-path
+                         clang-lookup-exe
+                         file line column))))
+    (let ((sym-file nil)
+          (sym-line nil)
+          (sym-column nil)
+          (lines (split-string output "\n")))
+      (dolist (line lines)
+        (let* ((tokens (split-string line "[=:]"))
+               (hd (nth 0 tokens)))
+          (when (equal "location" hd)
+            (setq sym-file (nth 1 tokens))
+            (setq sym-line (string-to-number (nth 2 tokens)))
+            (setq sym-column (string-to-number (nth 3 tokens))))))
+      (if (and sym-file sym-line sym-column)
+          (list :file sym-file :line sym-line :column sym-column)
+        (list :error "Symbol not found")))))
+
+(defun clang-goto-symbol-at-point ()
+  (interactive)
+  (condition-case nil
+      (destructuring-bind (:file file :line line :column column) (clang-lookup)
+        (message "loc %s:%s:%s" file line column)
+        (find-file file)
+        (goto-line line)
+        (beginning-of-line 1)
+        (forward-char (- column 1))
+        )
+    (error "Symbol not found")))
+
+(provide 'clang-lookup)
+
+
---
+++ clang_lookup_app.cpp
@@ -0,0 +1,79 @@
+///
+/// Simple CLI utility which will lookup the definition location of a symbol at
+/// a given source location
+///
+
+extern "C" {
+#include "clang-c/Index.h"
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( int argc, char* argv[] )
+{
+    if( argc < 3 )
+    {
+        printf("Expected lookup_symbol file line column");
+        return 1;
+    }
+
+    const char* file_name = argv[1];
+    const int line = atoi( argv[2] );
+    const int column = atoi( argv[3] );
+
+    CXIndex index = clang_createIndex( 0, 0 );
+    CXTranslationUnit tu = clang_parseTranslationUnit(
+        index, file_name, argv+3, argc-3, 0, 0, CXTranslationUnit_None );
+
+    bool had_errors = false;
+
+    for ( unsigned i = 0, n = clang_getNumDiagnostics(tu); i != n; ++i )
+    {    
+        CXDiagnostic diag = clang_getDiagnostic( tu, i );
+        CXString msg = clang_formatDiagnostic( diag,
+            clang_defaultDiagnosticDisplayOptions() );
+        fprintf(stderr, "%s\n", clang_getCString(msg) );
+        clang_disposeString( msg );
+
+        CXDiagnosticSeverity sev = clang_getDiagnosticSeverity( diag );
+        had_errors |= sev >= CXDiagnostic_Error;
+
+        // TODO: handle fixits
+        // clang_getDiagnosticNumFixIts, clang_getDiagnosticFixIt
+    }
+
+    CXString def_file_name_cx = { 0 };
+    unsigned def_line = 0, def_column = 0, def_offset = 0;
+    const char* def_file_name = 0;
+
+    if ( ! had_errors )
+    {
+        CXFile file = clang_getFile( tu, file_name );
+        CXSourceLocation lookup_loc = clang_getLocation( tu, file, line, column );
+        CXCursor sym = clang_getCursor( tu, lookup_loc );
+        CXCursor definition = clang_getCursorReferenced( sym );
+        CXSourceLocation def_loc = clang_getCursorLocation( definition );
+
+        CXFile def_file;
+        clang_getSpellingLocation( def_loc, &def_file, &def_line, &def_column, &def_offset );
+        def_file_name_cx = clang_getFileName( def_file );
+        def_file_name = clang_getCString( def_file_name_cx );
+    }
+
+    if( def_file_name != 0 )
+    {
+        printf( "location=%s:%d:%d\n", def_file_name, def_line, def_column );
+    }
+    else
+    {
+        printf( "location=\n" );
+    }
+
+    clang_disposeString( def_file_name_cx );
+    clang_disposeTranslationUnit( tu );
+    clang_disposeIndex( index );
+
+    return had_errors ? 1 : 0;
+}
+
